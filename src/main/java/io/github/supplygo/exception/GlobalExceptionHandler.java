c92e986a38f2981a799f2de08b6077f79a6eb65d
BigTyc <liux8023@foxmail.com>
2025-07-05 01:05:42 +0800

    添加参数校验

--- src/main/java/io/github/supplygo/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/supplygo/exception/GlobalExceptionHandler.java
@@ -2,18 +2,22 @@ package io.github.supplygo.exception;
 
 import io.github.supplygo.common.R;
 import io.github.supplygo.common.Status;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -28,6 +32,25 @@ public class GlobalExceptionHandler {
         log.warn("业务异常: code={}, message={}", status.getCode(), status.getMessage(), ex);
         // 自定义的业务异常类型，应该始终返回HTTP 200 OK
         return Mono.just(ResponseEntity.ok(R.fail(status)));
+    }
+
+    // 处理参数校验失败（@Valid + @RequestBody）
+    @ExceptionHandler({WebExchangeBindException.class})
+    public Mono<ResponseEntity<?>> handleMethodArgumentNotValid(WebExchangeBindException ex) {
+        String errorMsg = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining("，"));
+        log.error("参数校验异常: {}", errorMsg, ex);
+        return Mono.just(ResponseEntity.badRequest().body(R.fail(Status.PARAM_VALID_ERROR, errorMsg)));
+    }
+
+    //  处理约束失败（@Validated）
+    @ExceptionHandler(ConstraintViolationException.class)
+    public Mono<ResponseEntity<?>> handleConstraintViolation(ConstraintViolationException ex) {
+        log.error("约束校验异常: {}", ex.getMessage(), ex);
+        return Mono.just(ResponseEntity.badRequest().body(R.fail(Status.PARAM_VALID_ERROR, ex.getMessage())));
     }
 
     /**
